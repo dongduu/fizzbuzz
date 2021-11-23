8cc39a277e842278c738bc1f878a1677e8d3c519
dongduu <4012popo@naver.com>
2021-11-23 15:41:34 +0900

    fizzbuzz solution

---
+++ app.js
@@ -0,0 +1,13 @@
+
+for (i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log("fuzzbuzz");
+    } else if (i % 3 === 0) {
+        console.log ("fizz");
+    } else if (i % 5 === 0) {
+        console.log ("buzz");
+    } else {
+        console.log (i);
+    }
+}
+
