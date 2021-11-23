c99384986832c4c57feb00bbeee53851257a5b17
dongduu <4012popo@naver.com>
2021-11-23 15:45:01 +0900

    fizzbuzz solution(2)

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ for (i = 1; i <= 100; i++) {
     }
 }
 
+let array = [];
+
+for (i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        array.push("fuzzbuzz");
+    } else if (i % 3 === 0) {
+        array.push("fizz");
+    } else if (i % 5 === 0) {
+        array.push("buzz");
+    } else {
+        array.push(i);
+    }
+}
+
+console.log(array);
